90ef8fcbaea10a94d0749e13f69d09d41a8177e8
MBULLE CLODIA <mbulleclodiaetambong@gmail.com>
2023-05-16 05:00:21 +0100

    loop exercise 4

--- loop_exercise_4.py
+++ loop_exercise_4.py
@@ -1,1 +1,6 @@
 # Write a program in python that takes a list of numbers and returns the maximum number in the list
+
+list_of_numbers = [1,2,3,4,5,9,35]
+#f using the max() function to obtain the maximum number in the list:
+max_number = max(list_of_numbers)
+print("The maximum number in the list is: ", max_number)
