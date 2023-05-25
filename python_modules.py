96092fd7030ab55d2712acf53ba10390529bbbde
MBULLE CLODIA <mbulleclodiaetambong@gmail.com>
2023-05-25 04:27:18 +0100

    added python modules and exercises

---
+++ python_modules.py
@@ -0,0 +1,27 @@
+# modules consist of functions, variables
+
+# import build in module in python
+# importing math module
+import math
+# importing two modules at one
+import random
+print(math.sqrt(16))
+print(math.log(10))
+print(math.factorial(0))
+#print(random.randint(23,37))
+
+# importing power function
+base = 10
+power = 2
+print(math.pow(8, 2))
+
+# multiple modules can be imported at once by using comma to separate them
+# import math, random, os
+print(math.factorial(4), math.sqrt(3))
+
+import random as CLODIA
+print(CLODIA.randint(20,30))
+
+from math import sqrt as Akum
+print(Akum(47))
+
